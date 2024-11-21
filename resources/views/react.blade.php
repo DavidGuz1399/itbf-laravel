5dae3d1f70125e4c50337c4337233c2c149144cf
Edgar Escorcia <edgar.escorcia@gobiernobogota.gov.co>
2024-11-20 23:00:19 -0500

    Corrigiendo link de react

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -13,7 +13,7 @@
 
 <body>
     <div id="root"></div>
-    <script crossorigin src="../js/index-D52YZH9h.js"></script>
+    <script crossorigin src="js/index-D52YZH9h.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
